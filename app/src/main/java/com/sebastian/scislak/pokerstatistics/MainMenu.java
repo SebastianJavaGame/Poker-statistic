d7a094b33dc4525542895ef015549cb4d51d6d1d
Sebastian Ściślak <scislak98@wp.pl>
2018-01-21 19:33:27 +0100

    Pierwsza iteracja

---
+++ app/src/main/java/com/sebastian/scislak/pokerstatistics/MainMenu.java
@@ -0,0 +1,32 @@
+package com.sebastian.scislak.pokerstatistics;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void OpenWriteStatistics(View view) {
+        startActivity(new Intent(this, SaveData.class));
+    }
+
+    public void OpenShowStatistics(View view) {
+        startActivity(new Intent(this, ShowData.class));
+    }
+
+    public void OpenPokerTips(View view) {
+        Toast.makeText(this, "This option temporialiry unavaiable", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Exit(View view) {
+        System.exit(0);
+    }
+}
---
+++ app/src/main/java/com/sebastian/scislak/pokerstatistics/ShowData.java
@@ -0,0 +1,16 @@
+package com.sebastian.scislak.pokerstatistics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 2018-01-21.
+ */
+
+public class ShowData extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
