949c9199047e428c1174a36f5caab58559cddbd4
Sebastian Ściślak <scislak98@wp.pl>
2018-01-21 21:48:35 +0100

    Initial pre-saving avtivity

--- app/src/main/java/com/sebastian/scislak/pokerstatistics/SaveData.java
+++ app/src/main/java/com/sebastian/scislak/pokerstatistics/SaveData.java
@@ -2,6 +2,7 @@ package com.sebastian.scislak.pokerstatistics;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by User on 2018-01-21.
@@ -12,5 +13,11 @@ public class SaveData extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_data);
+    }
+
+    public void PreSavingInfo(View view) {
+    }
+
+    public void PreSaving(View view) {
     }
 }
